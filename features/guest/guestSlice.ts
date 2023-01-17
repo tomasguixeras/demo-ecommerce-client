70b42160e0dd2fd5f6b12df159de9884a96aa011
GenaBercini <bercinigenaro@gmail.com>
2023-01-16 21:21:20 -0300

    changes for Redux

--- features/guest/guestSlice.ts
+++ features/guest/guestSlice.ts
@@ -2,7 +2,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../app/store";
 
-const baseURL = `https://demo-ecommerce-api.onrender.com`;
+// const baseURL = `https://demo-ecommerce-api.onrender.com`;
+const baseURL = `http://localhost:3001`;
 
 export const fetchAllProducts = createAsyncThunk(
   "products/fetchAll",
@@ -14,10 +15,12 @@ export const fetchAllProducts = createAsyncThunk(
 
 interface GuestState {
   products: any[];
+  loading: boolean;
 }
 
 const initialState: GuestState = {
   products: [],
+  loading: false,
 };
 
 export const guestSlice = createSlice({
@@ -27,7 +30,11 @@ export const guestSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
       state.products = action.payload;
-    });
+      state.loading = false;
+    }),
+      builder.addCase(fetchAllProducts.pending, (state, action) => {
+        state.loading = true;
+      });
   },
 });
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
+import { Provider} from 'react-redux'
+import { store } from '../app/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <Provider store={store}>
     <ChakraProvider resetCSS={false} theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </Provider>
   )
 }
